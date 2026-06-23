a30708c27bf2fbdc7d6645fb4179a9ed9d035b24
hafizmaarif911-ux <hafizmaarif911@gmail.com>
2026-06-23 09:05:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,48 +8,28 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True
 
 bot = commands.Bot(
     command_prefix="!",
     intents=intents
 )
 
-# ==========================
-# READY
-# ==========================
-
-@bot.event
-async def on_ready():
-    try:
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} commands")
-    except Exception as e:
-        print(e)
-
-    print(f"Online : {bot.user}")
-    
-@bot.tree.error
-async def on_app_command_error(interaction, error):
-    print(f"ERROR COMMAND: {error}")
-# ==========================
-# PING
-# ==========================
+# =========================
+# PING COMMAND
+# =========================
 
 @bot.tree.command(
     name="ping",
-    description="Tes bot"
+    description="Test bot"
 )
 async def ping(interaction: discord.Interaction):
-
     await interaction.response.send_message(
-        "🏓 Pong!",
-        ephemeral=True
+        "🏹 Albion Party Bot Online!"
     )
 
-# ==========================
-# PARTY TEST
-# ==========================
+# =========================
+# PARTY COMMAND
+# =========================
 
 @bot.tree.command(
     name="party",
@@ -59,43 +39,47 @@ async def party(interaction: discord.Interaction):
 
     embed = discord.Embed(
         title="📢 AVA NEW ROUTE",
+        description="Leader: " + interaction.user.mention,
         color=0x5865F2
     )
 
     embed.add_field(
-        name="🛡 Main Tank",
+        name="Main Tank",
         value="Kosong",
         inline=False
     )
 
     embed.add_field(
-        name="💚 Holy",
+        name="Holy",
         value="Kosong",
         inline=False
     )
 
     embed.add_field(
-        name="⚔ DPS 1",
+        name="DPS",
         value="Kosong",
         inline=False
     )
-
-    embed.add_field(
-        name="⚔ DPS 2",
-        value="Kosong",
-        inline=False
-    )
-
-    embed.add_field(
-        name="👁 Scout",
-        value="Kosong",
-        inline=False
-    )
-
-    embed.set_footer(text="0/5 Slot")
 
     await interaction.response.send_message(
         embed=embed
     )
+
+# =========================
+# READY
+# =========================
+
+@bot.event
+async def on_ready():
+
+    print("Bot Login")
+
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+    print(f"Online : {bot.user}")
 
 bot.run(TOKEN)
