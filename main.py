e0210c97e38503357b9d5c4a1875787fff2340a0
hafizmaarif911-ux <hafizmaarif911@gmail.com>
2026-08-22 22:50:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 ALLOWED_CHANNELS = [
     1540389075247177909,
-	1518980919141077173,
-	1440327778451849277
+    1518980919141077173,
+    1440327778451849277
 ]
 
 intents = discord.Intents.default()
@@ -30,14 +30,15 @@ bot = commands.Bot(
 )
 
 parties = {}
+
 def channel_allowed(interaction):
     return interaction.channel.id in ALLOWED_CHANNELS
+
 # =====================================
 # EMBED
 # =====================================
 
 def build_embed(content_id):
-
     data = parties[content_id]
 
     embed = discord.Embed(
@@ -48,7 +49,6 @@ def build_embed(content_id):
     joined = 0
 
     for role in data["roles"]:
-
         user = data["members"][role]
 
         if user:
